13e4b6ea4e355fc61b92740bf3f1a7b3a972b3f5
123kishan <mishrakishan085@gmail.com>
2023-10-04 16:21:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Components/Layout/Header.js";
+import Meals from "./Components/Meals/Meals.js";
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Meals/>
+    </div>
+  );
+}
+
+export default App;
