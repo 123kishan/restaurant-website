0124f052e1d69d5f3c20a31797c5188e65b62cbf
123kishan <mishrakishan085@gmail.com>
2023-10-11 17:27:03 +0530

    adding add(plus) and remove(-) functionalities for cart items

--- src/Components/Meals/MealsItem/MealsItemForm.js
+++ src/Components/Meals/MealsItem/MealsItemForm.js
@@ -3,7 +3,7 @@ import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealsItemForm.module.css';
 
-const MealItemForm = (props) => {
+const MealsItemForm = (props) => {
   const [amountIsValid, setAmountIsValid] = useState(true);
   const amountInputRef = useRef();
 
@@ -45,4 +45,4 @@ const MealItemForm = (props) => {
   );
 };
 
-export default MealItemForm;
+export default MealsItemForm;
